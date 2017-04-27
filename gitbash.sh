e64ddfdbb323048b7ebb1350ceb07ac3a00ff0c1
Meazt <meazt.maezt@gmail.com>
2017-04-27 16:09:29 -0500

    New and improved

--- gitbash.sh
+++ gitbash.sh
@@ -1,45 +1,40 @@
 #!/bin/env bash
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
-#THIS IS INCOMPLETE
 
-#Run this script in the directory
-#Set Github username and password
-#e.g.
-#USERNAME="example"
-#PASSWORD="example12345"
-
-USERNAME=
-PASSWORD=
-
+USERNAME=$(git config user.name)
+GIT_PROFILE="https://github.com/"$USERNAME"/"
 #Set to 1 if you want a README.md file to created when you make a new git repo
 #Set to 0 if you don't want a README.md file
 README=0
-FIRST_COMMIT_MSG="\"First commit\""
+FIRST_COMMIT_MSG="\"Initial commit\""
+DATE=$(date +%Y-%m-%d)
 
-function createRepo {
-  repoName=$1
-  curl -u $USERNAME https://api.github.com/user/repos -d "{\"name\":\"$repoName\"}"
-  git init
-  if [README == 0]; then
-    touch README.md
-  fi
-  git add .
-  git commit -m ${FIRST_COMMIT_MSG}
-  git remote add origin "https://github.com/"$USERNAME"/"$repoName".git"
-  git push -u origin master
+function create {
+  for repo in "$@"; do
+    mkdir $repo
+    cd $repo
+    curl -u $USERNAME https://api.github.com/user/repos -d "{\"name\":\"$repo\"}"
+    git init
+    if [README -eq 0]; then
+      touch README.md
+    fi
+    git add .
+    git commit -m ${FIRST_COMMIT_MSG}
+    git remote add origin "$GIT_PROFILE""$repo"".git"
+    git push -u origin master
+    cd ..
+    echo "Creating $repo complete"
+  done
 }
 
-#Notice the 's' right after the word 'Repo'
-function createRepos {
-  NUM_OF_REPEATS = $# #How many repositories do you want to create?
-  for i in 1..NUM_OF_REPEATS; do
-    repoName=${$i}
-    createRepo repoName
+#loops through a list of repos and then pushes them into github
+function push {
+  for repo in "$@"; do
+    cd $repo
+    git add .
+    git commit -m "$DATE" #the commit message will be the current date
+    git remote add origin "$GIT_PROFILE""$repo"".git"
+    git push -u origin master
+    cd ..
+    echo "Repository $repo updated"
   done
 }
