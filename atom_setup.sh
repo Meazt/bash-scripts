3bb3fbd2a01dcb06a13b79d653d8ad76e7596300
Meazt <meazt.maezt@gmail.com>
2017-05-15 17:18:51 -0500

    fixed some things

--- atom_setup.sh
+++ atom_setup.sh
@@ -6,25 +6,29 @@
 #or if I have a lot of themes and packages installed and I don't want to
 #install each one of them one by one.
 
-#get operating system
-{
-  if [ -x /usr/bin/apm ]; then
-    echo "apm installed"
-    echo "installing apm themes"
-    apm install atom-material-ui
-    apm install atom-material-syntax
-    apm install nebula-ui
-    apm install nebula-syntax
-    echo "install packages"
-    apm install git-plus
-  else
-    echo "apm not installed"
-    echo "installing apm"
-    os="uname"
-    if [[ $os == "Darwin" ]]; then
-      echo "do stuff"
-    elif [[ $os == "Linux" ]]; then
-      echo "do stuff"
-    fi
-  fi
+function install_themes {
+  apm install atom-material-ui
+  apm install atom-material-syntax
+  apm install nebula-ui
+  apm install nebula-syntax
 }
+
+function install_packages {
+  apm install git-plus
+}
+
+if [ -x /usr/bin/apm ]; then
+  echo "installing apm themes"
+  install_themes
+  echo "installing packages"
+  install_packages
+else
+  echo "apm not installed"
+  echo "installing apm"
+  os="uname"
+  if [[ $os == "Darwin" ]]; then
+    echo "do stuff"
+  elif [[ $os == "Linux" ]]; then
+    echo "do stuff"
+  fi
+fi
--- gitbash.sh
+++ gitbash.sh
@@ -4,63 +4,62 @@
 #Create multiple repositories
 #Or delete multiple repositories
 #I may add some more features to this script so that it becomes more versatile
-{
-  USERNAME=$(git config user.name)
-  PASSWORD= #set PASSWORD to your password
-  GIT_PROFILE="https://github.com/"$USERNAME"/"
-  #if README is 0 then create() will create a README.md file
-  #else create() won't create a README.md file
-  README=0
-  FIRST_COMMIT_MSG="\"Initial commit\""
-  DATE=$(date +%Y-%m-%d)
 
-  echo "USERNAME: "$USERNAME
-  echo "PROFILE: "$GIT_PROFILE
+USERNAME=$(git config user.name)
+PASSWORD= #set PASSWORD to your password
+GIT_PROFILE="https://github.com/"$USERNAME"/"
+#if README is 0 then create() will create a README.md file
+#else create() won't create a README.md file
+README=0
+FIRST_COMMIT_MSG="\"Initial commit\""
+DATE=$(date +%Y-%m-%d)
 
-  function create {
-    for repo in "$@"; do
-      echo "MAKING $repo REPOSITORY"
-      mkdir $repo
-      cd ./$repo
-      curl -u $USERNAME:$PASSWORD https://api.github.com/user/repos -d "{\"name\":\"$repo\"}"
-      git init
-      if [[ $README -eq 0 ]]; then
-        touch README.md
-      fi
-      eval "git add ."
-      git commit -m "\"$FIRST_COMMIT_MSG\""
-      git remote add origin $GIT_PROFILE$repo.git
-      git push -u origin master
-      cd ..
-      echo "CREATING $repo REPOSITORY COMPLETE"
-    done
-  }
+echo "USERNAME: "$USERNAME
+echo "PROFILE: "$GIT_PROFILE
 
-  #loops through a list of repos and then pushes them into github
-  function push {
-    for repo in "$@"; do
-      echo "PUSHING TO $repo REPOSITORY"
-      cd $repo
-      eval "git add ."
-      git commit -m "\"$DATE\"" #the commit message will be the current date
-      git remote add origin $GIT_PROFILE$repo.git
-      git push -u origin master
-      cd ..
-      echo "$repo REPOSITORY PUSH COMPLETE"
-    done
-  }
-
-  function delete {
-    echo "delete"
-  }
-
-  function main {
-    git_function=$1
-    shift #removes the first argument which is the git function to use from the args list
-    args=$@
-    $git_function $args
-    git reset
-  }
-
-  main $@
+function create {
+	for repo in "$@"; do
+		echo "MAKING $repo REPOSITORY"
+		mkdir $repo
+		cd ./$repo
+		curl -u $USERNAME:$PASSWORD https://api.github.com/user/repos -d "{\"name\":\"$repo\"}"
+		git init
+		if [[ $README -eq 0 ]]; then
+			touch README.md
+		fi
+		eval "git add ."
+		git commit -m "\"$FIRST_COMMIT_MSG\""
+		git remote add origin $GIT_PROFILE$repo.git
+		git push -u origin master
+		cd ..
+		echo "CREATING $repo REPOSITORY COMPLETE"
+	done
 }
+
+#loops through a list of repos and then pushes them into github
+function push {
+	for repo in "$@"; do
+		echo "PUSHING TO $repo REPOSITORY"
+		cd $repo
+		eval "git add ."
+		git commit -m "\"$DATE\"" #the commit message will be the current date
+		git remote add origin $GIT_PROFILE$repo.git
+		git push -u origin master
+		cd ..
+		echo "$repo REPOSITORY PUSH COMPLETE"
+	done
+}
+
+function delete {
+	echo "delete"
+}
+
+function main {
+	git_function=$1
+	shift #removes the first argument which is the git function to use from the args list
+	args=$@
+	$git_function $args
+	git reset
+}
+
+main $@
